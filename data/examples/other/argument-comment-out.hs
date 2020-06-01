90dc7bf34d6333d94a87b72fb1a2af9429624215
Mark Karpov <markkarpov92@gmail.com>
2020-06-01 11:33:18 +0200

    Change rendering of type signatures

--- data/examples/other/argument-comment-out.hs
+++ data/examples/other/argument-comment-out.hs
@@ -1,16 +1,13 @@
-foo ::
-  -- | Documentation
-  Int ->
-  Bool
+foo
+  :: Int -- ^ Documentation
+  -> Bool
 foo _ = True
 
-foo ::
-  Foo a =>
-  -- | Foo
-  Int ->
-  Int
-foo ::
-  Foo a =>
-  -- | Foo
-  Int ->
-  Int
+foo
+  :: Foo a
+  => Int -- ^ Foo
+  -> Int
+foo
+  :: Foo a
+  => Int -- ^ Foo
+  -> Int
